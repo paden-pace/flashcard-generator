f8851076fda8968424fcd6a754b3e05de9e66d59
Paden Pace <pjpace27@yahoo.com>
2017-04-04 22:48:57 -0500

    basic files with example text

---
+++ cloze-card.js
@@ -0,0 +1,14 @@
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// "George Washington"
+console.log(firstPresidentCloze.cloze); 
+
+// " ... was the first president of the United States.
+console.log(firstPresidentCloze.partial); "
+
+// "George Washington was the first president of the United States.
+console.log(firstPresidentCloze.fullText): "
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+var brokenCloze("This doesn't work", "oops"); 
