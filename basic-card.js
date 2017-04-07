22088ee999559515cd200d305e951c97a563f4b1
Paden Pace <pjpace27@yahoo.com>
2017-04-07 10:37:57 -0500

    change of input format to a JSON object

--- basic-card.js
+++ basic-card.js
@@ -4,6 +4,49 @@ var fs = require('fs');
 var inquirer = require("inquirer");
 var request = require("request");
 
+var questions = [
+    {front: "What was the founding year for Muggle Quidditch?",
+    back: "2005"
+    },
+
+    {front: "How many active positions are on a pitch at a time?",
+    back: "7"
+    },
+
+    {front: "What position is identified with a white headband?",
+    back: "chaser"
+    },
+
+    {front: "What position is identified with a black headband?",
+    back: "beater"
+    },
+
+    {front: "What position is identified with a yellow headband?",
+    back: "seeker"
+    },
+
+    {front: "What position is identified with a green headband?",
+    back: "keeper"
+    },
+
+    {front: "What team won the Quidditch World Cup VI?",
+    back: "Texas Quidditch"
+    },
+
+    {front: "What team won the Quidditch World Cup VII?",
+    back: "Texas Quidditch"
+    },
+
+    {front: "What team won the Quidditch World Cup VIII?",
+    back: "Texas Quidditch"
+    },
+
+    {front: "Do the players actually fly?",
+    back: "no"
+    },
+];
+
+var score = 0;
 
 // var firstPresident = new BasicCard(
 //     "Who was the first president of the United States?", "George Washington");
@@ -15,35 +58,61 @@ var request = require("request");
 // console.log(firstPresident.back); 
 
 
+// Add a quiz function that will allow you to answer the question
+// add a score variable that will go up with correct ones 
+// Add a JSON file that has the cards  
+// Put the constructor function into a new file  
+
+
 function CardObj(back, front){
     this.back = back,
     this.front = front,
-    this.readBack = function(){
-        console.log("back: " + this.back);
+    this.incorrectBack = function(){
+        console.log("Incorrect. The correct answer is: " + this.back)
+        console.log("Your score is: " + score);
+        console.log("-------------------------------");
+    },
+    this.correctBack = function(){
+        console.log("Correct!");
+        score++; 
+        console.log("Your score is: " + score);
+        console.log("-------------------------------");
     },
     this.readFront = function(){
-        console.log("front: " + this.front);
+        console.log("-------------------------------");
+        console.log("Question: " + this.front);
     }
 };
 
-function incriment(i, data) {
-    if (i < (data.length-1)) {
+function incriment(i, questions) {
+    if (i < (questions.length)) {
         
-        console.log("test" + i);
-        var side = data[i].split("%");
-        var newCardObj = new CardObj(side[0], side[1]);
-        newCardObj.readBack();
+        //console.log("test" + i);
+        //var side = data[i].split("%");
+        //console.log(questions[i]);
+        var newCardObj = new CardObj(questions[i].back, questions[i].front);
+        newCardObj.readFront();
         inquirer.prompt([
             {
-                type: "confirm",
-                message: "Press [enter] to see the answer.",
-                name: "confirmation"
+                type: "input",
+                message: "What is your answer?",
+                name: "answer"
             }
         ]).then(function(review) {
-                newCardObj.readFront();
+            if(review.answer == newCardObj.back){
+                newCardObj.correctBack();
                 i++;
-                incriment(i, data);
+                incriment(i, questions);
+            } else if (review.answer != newCardObj.back){
+                newCardObj.incorrectBack();
+                i++;
+                incriment(i, questions);
+            };  
         });
+    } else if (i == (questions.length)){
+        console.log("-------------------------------");
+        console.log("Game Over! Your score was: " + score);
+        console.log("-------------------------------");
     };
 };
 
@@ -54,18 +123,18 @@ function options (){
         {
             type: "list",
             message: "What would you like to do?",
-            choices: ["create_new_card", "review_existing_cards", "exit"],
+            choices: ["review_existing_cards", "exit"],
             name: "selection"
         }
     ]).then(function(user) {
         switch (user.selection) {
-            case "create_new_card":
-                newCard();
+           // case "create_new_card":
+                //newCard();
                 //options();
-                break;
+                //break;
 
             case "review_existing_cards":
-                reviewCards();
+                incriment(0, questions);
                 //options();
                 break;
 
@@ -99,19 +168,19 @@ function newCard(){
     });
 };
 
-function reviewCards(){
+// function reviewCards(){
     
-  // To read the basic.txt file
-  fs.readFile("basic.txt", "utf8", function(err, data) {
+//   // To read the basic.txt file
+//   fs.readFile("basic.txt", "utf8", function(err, data) {
 
-    // Break down the text file to the individual cards
-    data = data.split("^");
+//     // Break down the text file to the individual cards
+//     data = data.split("^");
 
-    // using that data run the incriment function
-    incriment(0, data);
+//     // using that data run the incriment function
+//     incriment(0, questions);
 
-  });
+//   });
 
-};
+// };
 
 options();
